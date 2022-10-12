0015e697503afe6692e766efc94a73b436866e4d
murat <muratartan@hotmail.com>
2022-10-12 12:55:06 +0300

    set change team

--- programs/teamdao/src/lib.rs
+++ programs/teamdao/src/lib.rs
@@ -6,23 +6,36 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod teamdao {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, player_number:u8) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, name: String, player_number:u8) -> Result<()> {
         let team = &mut Account<Team> = &mut ctx.accounts.team;
         team.captain = ctx.accounts.signer.key();
+        team.name = name;
         team.players = 0;
         team.prize_owners = player_number
+
+        msg!("team {} is successfully created, captain is {}", team.name,team.captain );
+        Ok(())
+    }
+
+    pub fn change_team(ctx: Context<ChangeTeam>, name: String) -> Result<()> {
+        let team = &mut Account<Team> = &mut ctx.accounts.team;
+        team.captain = ctx.accounts.signer.key();
+        team.name = name;
+        msg!("team {} is successfully changed, the new captain is {}", team.name,team.captain );
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = signer,space = 8 + 8 + 2 + 32 + 1 + 32 )]
+    #[account(init, payer = signer,space = 8 + 8 + 2 + 32 + 1 + 64 )]
     pub team: Account<'info, Team>,
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>
 }
+
+
 
 pub struct Team {
     pub players: u8,
